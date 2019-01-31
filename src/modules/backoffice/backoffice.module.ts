c15f6f7a8669eb417136d342db65abaafe206e0e
I4PROINFO\anmak <andrew.mak@i4pro.com.br>
2019-01-31 20:04:01 -0200

    adicionando uma controller e novo modulo criado

---
+++ src/modules/backoffice/backoffice.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { CustomerController } from './controllers/customer.controller';
+
+@Module({})
+export class BackofficeModule {
+    controllers: [CustomerController]
+}
---
+++ src/modules/backoffice/controllers/customer.controller.ts
@@ -0,0 +1,19 @@
+import { Controller } from "@nestjs/common";
+
+@Controller()
+export class CustomerController {
+    get() {
+
+        return 'obter os clientes';
+    }
+
+    post() {
+        return 'obter os clientes';
+    }
+    put() {
+        return 'obter os clientes';
+    }
+    delete() {
+        return 'obter os clientes';
+    }
+}
